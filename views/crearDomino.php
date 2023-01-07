7aa4cd283f8920de95b21aba17d6471a4201bdad
Roxal13 <alvaroromfra@gmail.com>
2023-01-07 14:45:12 +0000

    Cambio de seguridad en el nivel de acceso

--- views/crearDomino.php
+++ views/crearDomino.php
@@ -1,17 +1,21 @@
 <?php
-include_once('includes/header.php');
+if($_SESSION['user']->getLevel() == 2) {
+  include_once('includes/header.php');
 ?>
-<form class="m-4" method="post">
-  <div class="form-group">
-    <label for="title">Titulo</label>
-    <input type="text" class="form-control" id="title" name="title">
-  </div>
-  <div class="form-group">
-    <label for="max">Número más alto de la ficha: </label>
-    <input type="number" class="form-control" id="max" name="max">
-  </div>
-<button type="submit" class="btn btn-primary my-3">Crear</button>
-</form>
+  <form class="m-4" method="post">
+    <div class="form-group">
+      <label for="title">Titulo</label>
+      <input type="text" class="form-control" id="title" name="title">
+    </div>
+    <div class="form-group">
+      <label for="max">Número más alto de la ficha: </label>
+      <input type="number" class="form-control" id="max" name="max">
+    </div>
+  <button type="submit" class="btn btn-primary my-3">Crear</button>
+  </form>
 <?php
+}else {
+  header("location: /index.php");
+}
 include_once('includes/footer.php');
 ?>
